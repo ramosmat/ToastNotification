080f79f69fdf7a23af13abb3a1edbe43e8f0e6e9
Matheus <matheusramosbi.13@gmail.com>
2023-11-14 15:34:22 -0300

    fonts.css href correction

--- src/index.js
+++ src/index.js
@@ -6,35 +6,43 @@ window.onload = () =>{
     const notificationButton = document.querySelector('button');
     const notificationSound = new Audio('/sounds/notification.wav')
 
-    //Criando variável que será o id da notificação quando clicado no botão "Mostrar Notificação"
-    const notificationId = 'notification-${Date.now()}';
-
+    
     //Criando evento que fica "escutando" a ação da variável notificationButton, esperando o click
     notificationButton.addEventListener("click", () => {
         //Criando viariavel newNotification, que será um clone do que existe na tag template
         const newNotification = notificationTemplate.content.cloneNode(true);
 
+        //Criando variável que será o id da notificação quando clicado no botão "Mostrar Notificação"
+        const notificationId = `notification-${Date.now()}`;
+
         //Criando variável que será o que existe na variável newNotification
         const dialog = newNotification.querySelector("dialog");
 
-        //Dando um id qualquer para a variável newNotification, será util para sumir com o alerta
+        //Dando um id para a variável dialog, será util para sumir com o alerta
         dialog.id = notificationId;
 
         dialog.addEventListener('close', (ev) => {
-            //Setado timeout nesta função para que o dialog fosse fechado na sextion após ser fechado, se não ia ficar um espaço em branco entre as notificações
+            //Timeout para que o dialog seja fechado na section cloque no close, se não ia ficar um espaço em branco entre as notificações
             setTimeout(() => {
                 ev.target.remove();
-            }, 700); //O tempo é o mesmo do duration na classe da tag dialog
-        });
+            }, 700) //O tempo é o mesmo do duration na classe da tag dialog
+        })
 
         //Inserindo no section o que está no newNotification, que é o que está na tag dialog
-        notificationSection.appendChild(newNotification);
+        notificationSection.appendChild(newNotification)
         notificationSound.play()
 
         //Setando timout para remover a notificação que aparecer após 5 segundos
+        // setTimeout(() => {
+        //     document.getElementById(notificationId).close();
+        // }, 3000);
+
         setTimeout(() => {
-            document.getElementById(notificationId).close();
-        }, 5000);
-    });
+            const dialogToClose = document.getElementById(notificationId)
+            if (dialogToClose) {
+                dialogToClose.close()
+            }
+        }, 4000)
+    })
 };
 
