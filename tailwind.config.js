e60a9d4d34d072c343869c0643789e7acfcd001b
Matheus <matheusramosbi.13@gmail.com>
2023-11-14 14:32:04 -0300

    adjustments on notification closeD transition

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["DM Sans", "sans-serif"],
+      },
+      animation: {
+        notification: "notification-enter 0.2s linear",
+      },
+      keyframes: {
+        "notification-enter": {
+          "0%": {transform: "translateX(18rem)", opacity: 0},
+          "100%": {transform: "translateX(0rem)", opacity: 100},
+        }
+      }
+    },
   },
   plugins: [],
 }
